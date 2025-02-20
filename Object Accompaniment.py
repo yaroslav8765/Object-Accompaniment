20f741f9200f9bb9b846cdc4964be57d7e0e4958
yaroslav8765 <pechorkin2014@gmail.com>
2025-02-20 18:40:45 +0200

    U can select active object

--- Object Accompaniment.py
+++ Object Accompaniment.py
@@ -3,12 +3,16 @@ from ultralytics import YOLO, ASSETS
 import numpy as np
 import cv2
 
-
+active_object_ID = 1
 model = YOLO("yolo11n.pt", task="detection")
 
 
-def detect_features(frame):
-    result = model.track(frame, conf=0.3, iou=0.5)[0]
+def detect_features(frame,active_object_ID):
+    result = model.track(frame, conf=0.5, iou=0.5)[0]
+    num_objects = len(result.boxes.data)
+    if active_object_ID > num_objects:
+        active_object_ID = num_objects
+
     for box in result.boxes.data: 
         x1, y1, x2, y2 = map(int, box[:4]) 
         conf = round(float(box[5]), 2)  
@@ -16,10 +20,9 @@ def detect_features(frame):
         class_name = model.names[int(cls)]
         track_id = int(box[4])
         if conf >= 0.50:
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    
-            cv2.rectangle(frame, (x1, y1), (x1+250, y1-30), (0, 0, 255), -1)
-            cv2.putText(frame,f"Id{track_id} {class_name} {conf}",(x1,y1),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=1,thickness=1,color=(255,255,255))
+            cv2.rectangle(frame, (x1, y1), (x2, y2),color= (255,0,0) if active_object_ID == track_id  else (0,0,255) ,thickness= 2)
+            cv2.rectangle(frame, (x1, y1), (x1+250, y1-30), color= (255,0,0) if active_object_ID == track_id  else (0,0,255), thickness=-1)
+            cv2.putText(frame,f"Id{track_id} {class_name} {conf}",(x1,y1),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=1,thickness=1,color=(255,255,255) )
     #if result.masks is not None:
     # for mask in result.masks:
     #    mask = mask.xy[0].astype(int) 
@@ -53,11 +56,18 @@ while True:
         print("No more stream :(")
         break
     
-    frame = detect_features(frame)
+    frame = detect_features(frame, active_object_ID)
 
     cv2.imshow("Webcam!", frame)
     
-    if cv2.waitKey(1) == ord('q'):
+    key = cv2.waitKey(1) 
+
+    if key == ord('w'):
+        active_object_ID += 1
+    elif key == ord('s'):
+        active_object_ID -= 1
+        active_object_ID = 0 if active_object_ID < 0 else active_object_ID
+    elif cv2.waitKey(1) == ord('q'):
         break  
 
 cv2.destroyAllWindows()
