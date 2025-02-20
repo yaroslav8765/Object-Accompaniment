823df1f6ba33b15908589754d9946ec46c6ec1a1
yaroslav8765 <pechorkin2014@gmail.com>
2025-02-20 19:13:45 +0200

    test commit

--- Object Accompaniment.py
+++ Object Accompaniment.py
@@ -12,7 +12,8 @@ def detect_features(frame,active_object_ID):
     num_objects = len(result.boxes.data)
     if active_object_ID > num_objects:
         active_object_ID = num_objects
-
+    #test comment##%$#%#%#%@@%%@%537457git 
+    
     for box in result.boxes.data: 
         x1, y1, x2, y2 = map(int, box[:4]) 
         conf = round(float(box[5]), 2)  
@@ -23,17 +24,7 @@ def detect_features(frame,active_object_ID):
             cv2.rectangle(frame, (x1, y1), (x2, y2),color= (255,0,0) if active_object_ID == track_id  else (0,0,255) ,thickness= 2)
             cv2.rectangle(frame, (x1, y1), (x1+250, y1-30), color= (255,0,0) if active_object_ID == track_id  else (0,0,255), thickness=-1)
             cv2.putText(frame,f"Id{track_id} {class_name} {conf}",(x1,y1),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=1,thickness=1,color=(255,255,255) )
-    #if result.masks is not None:
-    # for mask in result.masks:
-    #    mask = mask.xy[0].astype(int) 
-    #    mask_image = np.zeros_like(frame, dtype=np.uint8) 
-    #    cv2.fillPoly(mask_image, [mask], (255, 255, 255)) 
-
-    #    frame[mask_image[:, :, 0] > 0, 0] = 0 
-    #    frame[mask_image[:, :, 1] > 0, 1] = 0  
     return frame  
-
-
 
 
 
@@ -66,7 +57,7 @@ while True:
         active_object_ID += 1
     elif key == ord('s'):
         active_object_ID -= 1
-        active_object_ID = 0 if active_object_ID < 0 else active_object_ID
+        active_object_ID = 1 if active_object_ID <= 0 else active_object_ID
     elif cv2.waitKey(1) == ord('q'):
         break  
 
