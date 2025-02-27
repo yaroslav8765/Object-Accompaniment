9721de1c0b149dbcfc620d1c6d322a0c6bc47a7b
yaroslav8765 <pechorkin2014@gmail.com>
2025-02-27 17:32:08 +0200

    improved object accompaniment. Its possible to click on th eobject

--- Object Accompaniment.py
+++ Object Accompaniment.py
@@ -6,29 +6,77 @@ import cv2
 active_object_ID = 1
 model = YOLO("yolo11n.pt")
 
+click_position = None
+last_object_position = None
+last_object_class = None
 
-def detect_objects(frame,active_object_ID):
-    result = model.track(source = frame, conf=0.5, iou=0.5, show = False, persist=False)[0]
-    num_objects = len(result.boxes.data)
+def detect_objects(frame, active_object_coordinates):
+    global last_object_position  
+    global last_object_class
+    global click_position
+    result = model.track(source=frame, conf=0.01, iou=0.5, show=False, persist=False)[0]
 
-    if(len(result)<=0):
+    if not hasattr(result.boxes, "data") or result.boxes.data is None or len(result.boxes.data) == 0:
+        last_object_position = None 
         return frame
-    else:
-        for box in result.boxes.data: 
-            if len(box) < 7:
-                continue
-            x1, y1, x2, y2 = map(int, box[:4]) 
-            conf = round(float(box[5]), 2)  
-            class_name = model.names[int(box[6])]
-            track_id = int(box[4])
-            draw_rectangle(frame, x1, y1, x2, y2, class_name, conf, track_id)
-            cv2.putText(frame, f"Active object ID:{active_object_ID}", (frame.shape[0] - 150, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
-            if active_object_ID == track_id:
-                calculate_center_of_the_frame(frame.shape[0], frame.shape[1])
-                calulate_center_of_the_object(x1, y1, x2, y2)
-                cv2.line(frame, calculate_center_of_the_frame(frame.shape[1], frame.shape[0]), calulate_center_of_the_object(x1, y1, x2, y2), color=(0, 255, 0), thickness=2)
+
+    detected_objects = []
+    
+    for box in result.boxes.data :
+        if len(box) < 7:
+            continue
+
+        x1, y1, x2, y2 = map(int, box[:4])
+        track_id = int(box[4])
+        class_id = int(box[6])
         
-    return frame  
+        if class_id not in model.names:
+            continue
+
+        class_name = model.names[class_id]
+        center_x = (x1 + x2) // 2
+        center_y = (y1 + y2) // 2
+        detected_objects.append((track_id, class_name, (center_x, center_y), (x1, y1, x2, y2)))
+
+    if last_object_position is not None and click_position is None:
+        min_distance = float("inf")
+        best_match = None
+        for obj in detected_objects:
+            obj_center = obj[2]
+            distance = calculate_ditance_between_objects(frame.shape[0], frame.shape[1], obj_center[0], obj_center[1])
+            if distance < min_distance and obj[1] == last_object_class:
+                min_distance = distance
+                best_match = obj
+    
+        if best_match is not None:
+            object_id, class_name, center, bbox = best_match
+            last_object_position = center
+            last_object_class = class_name
+            draw_rectangle(frame, *bbox, class_name, 1.0, object_id)
+                
+    else:
+        if(click_position is not None):
+            min_distance = float("inf")
+            best_match = None
+            last_object_position = None
+            last_object_class = None
+            for obj in detected_objects:
+                obj_center = obj[2]
+                distance = calculate_ditance_between_objects(click_position[0], click_position[1], obj_center[0], obj_center[1])
+                if distance < min_distance:
+                    min_distance = distance
+                    best_match = obj
+
+            if best_match is not None:
+                object_id, class_name, center, bbox = best_match
+                last_object_position = center
+                last_object_class = class_name
+                draw_rectangle(frame, *bbox, class_name, 1.0, object_id)
+            click_position = None
+    
+    cv2.putText(frame, f"Click {click_position}", (frame.shape[0] - 150, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+
+    return frame
 
 def calulate_center_of_the_object(x1, y1, x2, y2):
     return (x1 + x2) // 2, (y1 + y2) // 2
@@ -44,9 +92,15 @@ def draw_rectangle(frame, x1, y1, x2, y2,class_name,conf,track_id):
 def calculate_ditance_between_objects(x1, y1, x2, y2):
     return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+def mouse_callback(event, x, y, flags, param):
+    global click_position
+    if event == cv2.EVENT_LBUTTONDOWN:
+        click_position = (x, y)
+
 
 stream = cv2.VideoCapture(0)
-
+cv2.namedWindow("Frame")
+cv2.setMouseCallback("Frame", mouse_callback)
 if not stream.isOpened():
     print("No stream :(")
     exit()
@@ -63,7 +117,7 @@ while True:
     frame = detect_objects(frame, active_object_ID)
     #result = model.track(source = frame, persist = False)
     #anotate_frame = result[0].plot()
-    cv2.imshow("Webcam!", frame)
+    cv2.imshow("Frame", frame)
     
     key = cv2.waitKey(1) 
 
@@ -72,7 +126,7 @@ while True:
     elif key == ord('s'):
         active_object_ID -= 1
         active_object_ID = 1 if active_object_ID <= 0 else active_object_ID
-    elif cv2.waitKey(1) == ord('q'):
+    elif key == ord('q'):
         break  
 
 cv2.destroyAllWindows()
